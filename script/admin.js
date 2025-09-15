3d61f5cf0d866acd474c9a6d7ef3213dbe23b125
rhayna <rhayna.casado@wr.sc.usp.br>
2025-09-15 00:19:23 -0300

    dashboard responsive

--- script/admin.js
+++ script/admin.js
@@ -214,10 +214,10 @@ const pageLog = document.getElementById('pageLog') !== null;
 
 if(pageLog){
   const logs = [
-    { data: '2025-08-24 10:12', nome: 'Maria Silva', atividade: 'Editou uma árvore.' },
-    { data: '2025-08-24 09:02', nome: 'João Pedro', atividade: 'Criou uma pergunta.' },
-    { data: '2025-08-23 22:41', nome: 'Ana Júlia', atividade: 'Ativou uma árvore.' },
-    { data: '2025-08-23 20:15', nome: 'Miguel Rodrigues', atividade: 'Desativou uma árvore.' }
+    { data: '2025-08-24 10:12', nome: 'Maria Silva', atividade: 'Editou uma árvore' },
+    { data: '2025-08-24 09:02', nome: 'João Pedro', atividade: 'Criou uma pergunta' },
+    { data: '2025-08-23 22:41', nome: 'Ana Júlia', atividade: 'Ativou uma árvore'},
+    { data: '2025-08-23 20:15', nome: 'Miguel Rodrigues', atividade: 'Desativou uma árvore' }
   ];
 
   const logBody = document.querySelector('#logTable tbody');
