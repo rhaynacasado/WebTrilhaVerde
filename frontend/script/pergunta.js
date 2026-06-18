67f0b4e11a263f55d816dc5e09728edef7e970cd
Rhayna Casado <rhaynacasado@usp.br>
2026-06-18 20:26:23 -0300

    Revert "teste2"
    This reverts commit 9f06def5f83cf9dceb33088ab7c28b6917167555.

--- frontend/script/pergunta.js
+++ frontend/script/pergunta.js
@@ -155,13 +155,9 @@
 
 
 
-
+      
     }
 
-
-
-
-    
     function makePlusSvg() {
       const svg = document.createElementNS('http://www.w3.org/2000/svg','svg');
       svg.setAttribute('viewBox','0 0 24 24');
